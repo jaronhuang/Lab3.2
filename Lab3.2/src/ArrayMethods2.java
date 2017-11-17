41ad4eb16db3c02158f70d31f9b9f78e449cb011
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-17 13:02:55 -0500

    .

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -3,8 +3,8 @@ public class ArrayMethods2
 {
 	public static void main(String[] args)
 	{
-		String[] list1 = {"abc", "bac", "afasdf"};
-		String[] list2 = {"abcde", "dea"};
+		String[] list1 = {"apple", "banana", "cherry", "dude", "elephant"};
+		String[] list2 = {"acorn", "cornnuts"};
 		printArray(merge(list1, list2));
 	}
 	
@@ -18,30 +18,51 @@ public class ArrayMethods2
 		int l2 = 0;
 		while (l1 + l2 < list3.length)
 		{
+			if (l1 == list1.length)
+			{
+				for (int i = l2; i < list2.length; i++)
+				{
+					list3[count] = list2[l2];
+					count++;
+					l2++;
+				}
+				break;
+			}
+			if (l2 == list2.length)
+			{
+				for (int i = l1; i < list1.length; i++)
+				{
+					list3[count] = list1[l1];
+					count++;
+					l1++;
+				}
+				break;
+			}
 			if ((list1[l1].compareTo(list2[l2])) < 0)
 			{
 				list3[count] = list1[l1];
 				count++;
 				l1++;
+				continue;
 			}
 			if ((list2[l2]).compareTo(list1[l1]) < 0)
 			{
 				list3[count] = list2[l2];
 				count++;
 				l2++;
+				continue;
 			}
-			if (l1 == list1.length)
-			{
-				list3[count] = list2[l2];
-				l2++;
-				count++;
-			}
-			if (l2 == list2.length)
+			if ((list1[l1].compareTo(list2[l2])) == 0)
 			{
 				list3[count] = list1[l1];
-				l1++;
 				count++;
+				l1++;
+				list3[count] = list2[l2];
+				count++;
+				l2++;
+				continue;
 			}
+			
 		}
 		return list3;
 		
@@ -50,7 +71,10 @@ public class ArrayMethods2
 	
 	public static String[] mergeSort(String[] list)
 	{
-
+		if (list.length == 1)
+		{
+			return list;
+		}
 	}
 	
 	public static int partition(int[] list)
