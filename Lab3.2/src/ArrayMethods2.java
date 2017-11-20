fc85748e352f373b85e8487e14c9937a2b3e0687
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-20 13:14:09 -0500

    .

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -3,9 +3,12 @@ public class ArrayMethods2
 {
 	public static void main(String[] args)
 	{
-		String[] list1 = {"apple", "banana", "cherry", "dude", "elephant"};
-		String[] list2 = {"acorn", "cornnuts"};
-		printArray(merge(list1, list2));
+		//String[] list1 = {"apple", "banana", "cherry", "dude", "elephant"};
+		//String[] list2 = {"acorn", "cornnuts"};
+		//printArray(merge(list1, list2));
+		
+		int[] list3 = {4, 6, 1, 3, 4, 6, 3};
+		System.out.println(partition(list3));
 	}
 	
 	public static String[ ] merge(String[] list1, String[] list2)
@@ -79,7 +82,48 @@ public class ArrayMethods2
 	
 	public static int partition(int[] list)
 	{
+		int pivot = countLarge(list);
+		swap(list, 0, pivot);
 		
+		/*for (int i = 0; i < pivot; i++)
+		{
+			if (list[i] <= pivot)
+			{
+				continue;
+			}
+			else
+			{
+				for (int j = pivot + 1; j < list.length; i++)
+				{
+					if (list[j] <= pivot)
+					{
+						swap(list, i, j);
+					}
+				}
+			}
+		}*/
+		
+		return pivot;
+	}
+	
+	public static int countLarge(int[] list)
+	{
+		int count = 0;
+		for (int i = 1; i < list.length; i++)
+		{
+			if (list[0] < list[i])
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void swap(int[] arr, int i, int j)
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 	
 	public static void printArray(String[] arr)
@@ -91,4 +135,12 @@ public class ArrayMethods2
 		System.out.println();
 	}
 	
+	public static void printArray(int[] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print("[" + arr[i] + "] ");
+		}
+		System.out.println();
+	}
 }
